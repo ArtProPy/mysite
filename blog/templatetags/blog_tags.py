196b5bef8749011fcbaeaaa78f14398f90c0eb47
ArtProPy <89776481+ArtProPy@users.noreply.github.com>
2021-09-06 20:16:26 +0300

    Initial commit

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,30 @@
+from django import template
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
+
+from blog.models import Post
+
+register = template.Library()
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_post(count=5):
+    latest_post = Post.published.order_by('-publish')[:count]
+    return {'latest_post': latest_post}
+
+
+@register.inclusion_tag('blog/post/most_comment_posts.html')
+def get_most_commented_posts(count=5):
+    most_commented_posts = Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+    return {'most_commented_posts': most_commented_posts}
