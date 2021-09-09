e9a07270cf6a2470c68afacc7cc738e2f146e67b
Artpro <ArtProPy@gmail.com>
2021-09-09 14:19:46 +0300

    Add docker and linter

---
+++ mysite/docker.py
@@ -0,0 +1,16 @@
+"""
+Module indented to extend and override settings.py via the environment
+variables.
+"""
+
+import os
+
+from datetime import timedelta
+
+from mysite.settings import *
+
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_POST = os.getenv('EMAIL_POST', '')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', '').lower() == 'true'
