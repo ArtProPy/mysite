4e2e93bc4174f0b7c0b5ef9be604cf7a66a16dfa
ArtProPy <89776481+ArtProPy@users.noreply.github.com>
2021-09-16 11:26:35 +0300

    Update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.docker')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.docker')
 
 application = get_wsgi_application()
